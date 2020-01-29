6712b7d0fb2f18000dfd3854d5f95613807c0c38
eels <ely.eels@gmail.com>
2020-01-29 16:59:43 -0500

    adds glob, strips special characteres

--- logfile-converter.py
+++ logfile-converter.py
@@ -20,9 +20,34 @@
         # print(line)
         # revline = line.strip()
         # print(revline)
+        # s = re.sub('[^0-9a-zA-Z]+', '', s)
 
-loglines = []
-with open ('cosnolelog.txt', 'rt') as logfile:
-    for logline in logfile:
-        loglines.append(logline)
-print(loglines) 
+import re
+import glob
+
+# open the log files
+all_logs = glob.glob('*.txt')
+
+log_lines = []
+
+with open (all_logs[0], 'rt') as log_file:
+    log = log_file.read()
+    log = re.sub('[-=<>]', '', log)
+    log.split('\n')
+
+    # the above is working to remove special characters, unclear if the split is happening, suggest writing to file to check
+
+    for log in log_file:
+
+        log_lines.append(log)
+    # for log_line in log_file:
+
+        # first, let's remove characters we don't need
+        # line_clean = re.sub('[-=<>]', '', log_line)
+        # str.lower(line_clean)
+
+        # log_lines.append(line_clean)
+        
+# only print the lines we need - we will use this later
+# print(log_lines[1:-1])
+print(log_lines)
