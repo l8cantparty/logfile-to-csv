79d04e58cdfe2a0343382199a6bc70418d1624cf
eels <ely.eels@gmail.com>
2020-01-31 16:21:14 -0500

    reworking

--- logfile-converter.py
+++ logfile-converter.py
@@ -1,45 +1,53 @@
-
-# logfile = store the content of the text file
-# to do: replace explict file name with glob
-
-# open text file as read-only
-# strip_text = ['=','-']
-
-
-# with open('cosnolelog.txt', 'rt') as logfile:
-    # log = logfile.read()
-
-    # strip_text = ['Dump', 'Ticks']
-    # print(log.strip())
-# logfile = "adsflkdjfljs===a"
-    # for line in logfile:
-        # print(line)
-    # print (logfile.strip( 'Ticks' ))
-    # for line in logfile:
-    # print to see that it's working
-        # print(line)
-        # revline = line.strip()
-        # print(revline)
-        # s = re.sub('[^0-9a-zA-Z]+', '', s)
-
 import re
+import csv
 import glob
 
-# open the log files
+# find the log files to open
 all_logs = glob.glob('*.txt')
 
 log_lines = []
 
+# open the log files as read only
 with open (all_logs[0], 'rt') as log_file:
-    log = log_file.read()
-    log = re.sub('[-=<>]', '', log)
-    log.split('\n')
 
+    # read the files and store as "log"
+    log = log_file.read()
+
+    # for log in log_file:
+
+    # split text at linebreaks
+    log_line = log.split('\n')
+    log_line = re.sub('[-=<>]', '', log)
+    # log_line[4:8]
+
+    for log in log_line:
+
+        print(log)
+
+
+
+filename = 'log-file' + '.csv'
+new_csv = open(filename, 'w')
+
+new_csv.write(log_line)
+new_csv.close()
+
+
+    # title, handle = s.split()
+
+
+        # log = log_file.read()
+
+        # header, cells = log.split(':')
+        # log
+        # log = re.sub('[-=<>]', '', log)
+        # log.split('\n')
+        # print(log)
     # the above is working to remove special characters, unclear if the split is happening, suggest writing to file to check
 
-    for log in log_file:
+    # for log in log_file:
 
-        log_lines.append(log)
+        # log_lines.append(log)
     # for log_line in log_file:
 
         # first, let's remove characters we don't need
@@ -50,4 +58,6 @@ with open (all_logs[0], 'rt') as log_file:
         
 # only print the lines we need - we will use this later
 # print(log_lines[1:-1])
-print(log_lines)
+    
+
+
