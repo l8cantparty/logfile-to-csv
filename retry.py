2a52456245078a8b46a6915a69889be4cd7889bc
l8cantparty <ely.eels@gmail.com>
2020-02-06 23:05:24 -0500

    experiments with changing strings

--- retry.py
+++ retry.py
@@ -10,30 +10,36 @@ all_logs = glob.glob('*.txt')
 # open the log files as read only
 with open (all_logs[0], 'r') as log_file:
 
-    log_reader = log_file.readlines()
+    log_list = log_file.readlines()
 
-    # line = re.sub(r'.*>', '', line)
+    log_list = re.sub(r'.*>', '', log_list)
 
-    clean_log = str.maketrans("", "", "=-<>")
+    clean_string = str.maketrans("", "", "=-<>")
 
-    log_reader = [s.translate(clean_log) for s in log_reader]
+    log_list = [s.translate(clean_string) for s in log_list]
 
-    # clean_list = str()
+    # log_list = list(filter('MSDKDecoderWithMCDemux', log_list)) 
     # remove lines with uneeded content
     # figure out how to combine these
-    log_reader = [x for x in log_reader if "MSDKDecoderWithMCDemux" not in x]
-    log_reader = [x for x in log_reader if "Dump" not in x]
+
+    # consider using filter
+
+    log_list = [x for x in log_list if "MSDKDecoderWithMCDemux" not in x]
+    log_list = [x for x in log_list if "Dump" not in x]
+    # log_list = log_list.lsplit('0')
 
 # >>> stringlist[:] = [x for x in stringlist if "Two" not in x]
 # >>> stringlist
 # ['elementOne', 'elementThree']
+# s = "this    is my long     sentence"
+# print ' '.join(s.split(' ')[3:])
    
-    print(log_reader[3:-5])
+    print(log_list[3:-5])
 
 # match name of log file
 filename = 'log-file' + '.csv'
 new_csv = open(filename, 'w')
 
 # double check that this is always the same distance
-new_csv.writelines(log_reader[3:-5])
+new_csv.writelines(log_list[3:-5])
 new_csv.close()
